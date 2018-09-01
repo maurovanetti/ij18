05f660b1efa97e8a62b1bbb35b121782b1fc9dfb
Mauro Vanetti <posta@maurovanetti.info>
2018-09-01 19:15:07 +0200

    Working engine prototype using dummy modules

---
+++ Assets/Scripts/Dummy/DummyMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyMap : MonoBehaviour, IMap {
+
+    public void MoveMarker(int channel, Vector2 position)
+    {
+        Debug.Log(channel + ": MARKER @ " + position);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/IMap.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public interface IMap
+{
+    void MoveMarker(int channel, Vector2 position);
+}
