bbf8c272e41ce7c5097cfde19d9a15e436070aa6
Davide Bosco <boscodavide25@gmail.com>
2018-09-02 12:55:53 +0200

    UI Added, Main Scene Added, Interfaces Updated

---
+++ Scripts/IMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public interface IMap
+{
+    void UpdateMap(int channel);
+    void MoveMarker(int channel, Vector2 position);
+}
